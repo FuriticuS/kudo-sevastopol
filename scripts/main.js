d46a92bc0951abc0068291c799480dbdac6c83fd
Fur1ouS <Fur1ouS@ya.ru>
2025-05-14 16:45:17 +0300

    add fullpage and header

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new fullpage('#fullpage', {
+        navigation: false,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+    });
+});
